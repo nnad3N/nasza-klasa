8531c9ee0803ba2e35fc43fa43a74352d78b69af
Damian Struk <nnad3n@outlook.com>
2021-11-06 12:26:14 +0100

    Init

---
+++ app.js
@@ -0,0 +1,107 @@
+const form = document.querySelector('#form');
+const formInputs = document.querySelectorAll('.form-control');
+const formName = document.querySelector('#form-name');
+const formSurname = document.querySelector('#form-surname');
+const formEmail = document.querySelector('#form-email');
+const formMessage = document.querySelector('#form-message');
+const formButton = document.querySelector('#form-button');
+
+let formError = false;
+
+const getFormValues = () => {
+	formError = false;
+
+	const { sanitizedName, sanitizedSurname, sanitizedEmail, sanitizedMessage } =
+		sanitizeFormValues();
+
+	return {
+		name: sanitizedName,
+		surname: sanitizedSurname,
+		email: sanitizedEmail,
+		message: sanitizedMessage,
+	};
+};
+
+const sanitizeFormValues = () => {
+	return {
+		sanitizedName: validator.escape(`${formName.value}`),
+		sanitizedSurname: validator.escape(`${formSurname.value}`),
+		sanitizedEmail: validator.escape(`${formEmail.value}`),
+		sanitizedMessage: validator.escape(`${formMessage.value}`),
+	};
+};
+
+const validateFormValues = () => {
+	const { sanitizedName, sanitizedSurname, sanitizedEmail, sanitizedMessage } =
+		sanitizeFormValues();
+	const name = validator.isEmpty(sanitizedName)
+		? errorHandler(formName, 'We just want to know your name')
+		: sanitizedName;
+
+	const surname = validator.isEmpty(sanitizedSurname)
+		? errorHandler(formSurname, 'And your surname will be handy')
+		: sanitizedSurname;
+
+	const email =
+		validator.isEmpty(sanitizedEmail) || !validator.isEmail(sanitizedEmail)
+			? errorHandler(
+					formEmail,
+					'But please enter a valid email so we can contact you'
+			  )
+			: sanitizedEmail;
+
+	const message = validator.isEmpty(sanitizedMessage)
+		? errorHandler(formMessage, 'And give us some words about you')
+		: sanitizedMessage;
+
+	return {
+		name,
+		surname,
+		email,
+		message,
+	};
+};
+
+const errorHandler = (place, err) => {
+	formError = true;
+	place.className = 'error';
+	formButton.className = 'error';
+	place.value = err;
+};
+
+const handleFormInputs = () => {
+	if (formError) {
+		const errors = document.querySelectorAll('.error');
+		errors.forEach((error) => {
+			error.classList.remove('error');
+			error.value = '';
+		});
+		formError = false;
+	}
+};
+
+const encode = (data) => {
+	return Object.keys(data)
+		.map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+		.join('&');
+};
+
+const submitForm = (e) => {
+	e.preventDefault();
+	const formValues = getFormValues();
+	validateFormValues();
+
+	if (formError) {
+		console.log('formError');
+	} else {
+		console.log(formValues);
+		let formData = encode(formValues);
+		console.log(formData);
+	}
+};
+
+form.addEventListener('change', getFormValues);
+form.addEventListener('submit', submitForm);
+formInputs.forEach((formInput) =>
+	formInput.addEventListener('click', handleFormInputs)
+);
